b2fab6862c9abbb94e56843bce5e3eca988a083f
Nathan <savardnm@gmail.com>
2026-01-31 15:52:29 -0500

    spotlights selectable

---
+++ _scripts/spotlight.gd
@@ -0,0 +1,43 @@
+extends Area2D
+class_name Spotlight
+
+@export var size = 100
+
+@export var color = Color(1,1,1,1):
+	set(value):
+		set_spotlight_color(value)
+		color = value
+
+@onready var target_position = global_position
+var drag = 0.85
+var mass = 1.0
+var velocity = Vector2.ZERO
+
+var outline = false:
+	set(value):
+		outline = value
+		queue_redraw()
+
+func set_spotlight_color(new_color):
+	var sprite: Sprite2D = find_child("Color Circle")
+	sprite.material.set_shader_parameter("objectColor", new_color)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_spotlight_color(color)
+	pass # Replace with function body.
+
+func draw_outline():
+	draw_circle(Vector2.ZERO, size, Color.WHITE, false, 2)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float):
+	var direction = target_position - global_position
+	velocity += direction / mass
+	velocity = velocity * drag
+	position += velocity * delta
+	pass
+
+func _draw():
+	if outline:
+		draw_outline()

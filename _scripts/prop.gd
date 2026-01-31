d84fd385d1b5c20363fa67e76aed9a7d9b6f1f96
unknown <dvdonato@gmail.com>
2026-01-31 16:00:56 -0500

    loading puzzles and checking completion

---
+++ _scripts/prop.gd
@@ -0,0 +1,17 @@
+extends Node
+
+
+@onready var viewport : Viewport  
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	viewport = get_tree().root 
+	pass # Replace with function body
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func get_current_color():
+	return viewport.get_texture().get_image().get_pixel(self.position.x,self.position.y) 

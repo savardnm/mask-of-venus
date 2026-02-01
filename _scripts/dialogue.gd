f754c97c5c6deb43043e29133bd8b832445eae7b
Nathan <savardnm@gmail.com>
2026-01-31 19:53:29 -0500

    text and curtains

--- _scripts/dialogue.gd
+++ _scripts/dialogue.gd
@@ -1,16 +1,18 @@
 extends Control
 class_name SceneDialogue
 
-
+@export var autoread = false
 @export_multiline var puzzle_prompt_text: String
 @export_multiline var puzzle_complete_text: String
+@export_multiline var easter_egg_text: String
 @onready var text_display: RichTextLabel = find_child("Text Display")
 
 var text_read_speed = 10
 var read_progress = 0.0
 
+var phase = 0
+
 func _ready():
-	text_display.text = "[color=black]" + "BBBBBBBBBBBB" + "[/color]"
 	give_prompt()
 	pass
 
@@ -20,17 +22,38 @@ func _process(delta: float):
 		text_display.visible_characters = round(read_progress)
 	else:
 		text_display.visible_ratio = 1.0
+
+		match phase:
+			0:
+				prompt_finished.emit()
+				phase = 1
+			2:
+				complete_finished.emit()
+				phase = 3
+			4:
+				easter_egg_finished.emit()
+				phase = 5
 	
 
 func give_prompt():
 	read_text(puzzle_prompt_text)
-	pass
+	phase = 0
+	
+func give_complete():
+	read_text(puzzle_complete_text)
+	phase = 1
+
 
 func puzzle_complete():
 	pass
 
 func read_text(string: String):
-	string = "[color=black]" + string + "[/color]"
+	# string = "[color=black]" + string + "[/color]"
 	read_progress = 0.0
 	text_display.visible_characters = round(read_progress / string.length())
 	text_display.text = string
+
+
+signal prompt_finished
+signal complete_finished
+signal easter_egg_finished
